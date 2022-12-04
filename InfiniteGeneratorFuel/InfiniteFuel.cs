414edf6694b27e8874add94eb3099caaabc2efd3
Maker-Macher <63427357+Maker-Macher@users.noreply.github.com>
2022-12-04 17:58:47 +0100

    Add project files.

---
+++ InfiniteGeneratorFuel/InfiniteFuel.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniStorm;
+using UnityEngine;
+
+namespace InfiniteGeneratorFuel
+{
+    [BepInPlugin("forza.infinitefuel", "InfinteFuel", "1.0")]
+    public class InfiniteFuel : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            new Harmony("forza.infinitefuel").PatchAll();
+
+            Logger.LogMessage("Loaded and patched");
+        }
+    }
+
+    [HarmonyPatch(typeof(HomeGenerator), "Tick")]
+    public class Patch
+    {
+        [HarmonyPrefix]
+        public static bool TickPatch(int tick, HomeGenerator __instance, ref Part_FuelTank ___FuelTank)
+        {
+            ___FuelTank.Get_FuelLevel = ___FuelTank.Get_FuelCapacity;
+
+            return true;
+        }
+    }
+}
